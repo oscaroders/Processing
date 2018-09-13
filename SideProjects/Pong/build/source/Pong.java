22728e042a8933613acb11681ba9eb03301a4be2
oscaroders <oscar.oders@gmail.com>
2018-09-13 10:38:33 +0200

    impossible to win!!!

--- SideProjects/Pong/build/source/Pong.java
+++ SideProjects/Pong/build/source/Pong.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Pong extends PApplet {
+
 boolean gameStart = false;
 int windowWidth = 1000;
 int windowHeight = 500;
@@ -26,12 +42,12 @@ int oppositePoint = 0;
 String oppositeScore;
 
 
-void setup()
+public void setup()
 {
-  size(1000, 500);
+  
 }
 
-void draw()
+public void draw()
 {
     background(255);
 
@@ -97,12 +113,12 @@ void draw()
 
 }
 
-void mousePressed()
+public void mousePressed()
 {
     gameStart = !gameStart;
 }
 
-void pointCounter(int points)
+public void pointCounter(int points)
 {
     points = points+1;
     String score = "zero";
@@ -125,12 +141,12 @@ void pointCounter(int points)
 
     int textHeight = 62;
     textSize(textHeight);
-    color d = color(oppositeColor[0], oppositeColor[1], oppositeColor[2], scoreAlpha);
+    int d = color(oppositeColor[0], oppositeColor[1], oppositeColor[2], scoreAlpha);
     fill(d);
     text(score, (width/4) - 60, (height/2) + 15);
 
     noStroke();
-    color c = color(playersColor[0], playersColor[1], playersColor[2], scoreAlpha);
+    int c = color(playersColor[0], playersColor[1], playersColor[2], scoreAlpha);
     fill(c);
     text("zero", (width*3/4) - 60, (height/2) + 15);
 
@@ -140,4 +156,14 @@ void pointCounter(int points)
     }
 
 
+}
+  public void settings() {  size(1000, 500); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Pong" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
