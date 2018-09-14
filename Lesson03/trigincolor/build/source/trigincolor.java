7d545cf6691e994b52fc65f794afd43285908e26
oscaroders <oscar.oders@gmail.com>
2018-09-14 15:41:42 +0200

    cleand up in code

--- Lesson03/trigincolor/build/source/trigincolor.java
+++ Lesson03/trigincolor/build/source/trigincolor.java
@@ -49,13 +49,8 @@ public void draw()
 	point(width - 25, height - 25);
 
   for(float angle = 0; radians(angle) < 2*PI; angle += 360/numberOfPoints){
-
-
-
     strokeWeight(1);
     stroke(128 + sin(angle)*100, 128 + cos(frame) * 100, 128 + tan(circleRad));
-    //point(center[0] + cos(radians(angle + frame)) * circleRad, center[1] + (sin(radians(angle + frame)) * circleRad));
-    //point(center[0] + cos(radians(angle + lineDegreeLengt + frame)) * circleRad, center[1] + (sin(radians(angle + lineDegreeLengt + frame)) * circleRad));
     LineCord connectLine = new LineCord(center[0] + cos(radians(angle - frame)) * circleRad*1.5f,
                                         center[1] + (sin(radians(angle - frame)) * circleRad*1.5f),
                                         center[0] + cos(radians(angle + lineDegreeLengt - frame)) * circleRad*1.5f,
@@ -68,59 +63,44 @@ public void draw()
     ellipse(center[0], center[1], circleDiam, circleDiam);
   }
 
-
   strokeWeight(3);
   stroke(255, 255, 0);
   fill(230, 0, 0);
   ellipse(center[0], center[1], circleDiam, circleDiam);
 
-    for(float angle = 0; radians(angle) < PI/2; angle += 90/numberOfPoints){
-
+		for(float angle = 0; radians(angle) < PI/2; angle += 90/numberOfPoints){
       strokeWeight(1);
-      //point(center[0] + cos(radians(angle + frame +90)) * circleRad, center[1] + (sin(radians(angle + frame +90)) * circleRad));
-      //point(center[0] + cos(radians(angle + lineDegreeLengt + frame +90)) * circleRad, center[1] + (sin(radians(angle + lineDegreeLengt + frame +90)) * circleRad));
       LineCord connectLine = new LineCord(center[0] + cos(radians(angle + frame +90)) * circleRad,
                                           center[1] + (sin(radians(angle + frame +90)) * circleRad),
                                           center[0] + cos(radians(angle + lineDegreeLengt + frame +90)) * circleRad,
                                           center[1] + (sin(radians(angle + lineDegreeLengt + frame +90)) * circleRad));
 
       line(connectLine.x1, connectLine.y1, connectLine.x2, connectLine.y2);
-      //frame += 0.02;
+
 		}
 
     for(float angle = 0; radians(angle) < PI/2; angle += 90/numberOfPoints){
-
       strokeWeight(1);
-      //point(center[0] + cos(radians(angle + frame +90)) * circleRad, center[1] + (sin(radians(angle + frame +90)) * circleRad));
-      //point(center[0] + cos(radians(angle + lineDegreeLengt + frame +90)) * circleRad, center[1] + (sin(radians(angle + lineDegreeLengt + frame +90)) * circleRad));
       LineCord connectLine = new LineCord(center[0] + cos(radians(angle + frame -90)) * circleRad,
                                           center[1] + (sin(radians(angle + frame -90)) * circleRad),
                                           center[0] + cos(radians(angle + lineDegreeLengt + frame -90)) * circleRad,
                                           center[1] + (sin(radians(angle + lineDegreeLengt + frame -90)) * circleRad));
 
       line(connectLine.x1, connectLine.y1, connectLine.x2, connectLine.y2);
-      //frame += 0.02;
     }
 
     for(float angle = 0; radians(angle) < PI/2; angle += 90/numberOfPoints){
-
       strokeWeight(1);
-      //point(center[0] + cos(radians(angle + frame +90)) * circleRad, center[1] + (sin(radians(angle + frame +90)) * circleRad));
-      //point(center[0] + cos(radians(angle + lineDegreeLengt + frame +90)) * circleRad, center[1] + (sin(radians(angle + lineDegreeLengt + frame +90)) * circleRad));
       LineCord connectLine = new LineCord(center[0] + cos(radians(angle + frame -180)) * circleRad,
                                           center[1] + (sin(radians(angle + frame -180)) * circleRad),
                                           center[0] + cos(radians(angle *2 + lineDegreeLengt + frame -180)) * circleRad,
                                           center[1] + (sin(radians(angle *2 + lineDegreeLengt + frame -180)) * circleRad));
 
       line(connectLine.x1, connectLine.y1, connectLine.x2, connectLine.y2);
-      //frame += 0.02;
     }
 
     for(float angle = 0; radians(angle) < PI/2; angle += 90/numberOfPoints){
-
       strokeWeight(1);
-      //point(center[0] + cos(radians(angle + frame +90)) * circleRad, center[1] + (sin(radians(angle + frame +90)) * circleRad));
-      //point(center[0] + cos(radians(angle + lineDegreeLengt + frame +90)) * circleRad, center[1] + (sin(radians(angle + lineDegreeLengt + frame +90)) * circleRad));
       LineCord connectLine = new LineCord((center[0] + -1*cos(radians(angle - frame +180)) * circleRad),
                                           (center[1] + (sin(radians(angle - frame +180)) * circleRad)),
                                           (center[0] + -1*cos(radians(angle *2 + lineDegreeLengt - frame +180)) * circleRad) ,
